cf2ea088946ae6f541159e0a37d2fa6aa0b4c23b
devgrv <dgourav1992@gmail.com>
2019-09-26 11:48:08 +0530

    Day1 - ch3

---
+++ Devworkxlabs/Amicus_Training/first-app/src/app/hard/hard.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-hard',
+  templateUrl: './hard.component.html',
+  styleUrls: ['./hard.component.scss']
+})
+export class HardComponent {
+  title = 'first-app';
+}
